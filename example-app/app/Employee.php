8df39d935a69eae17af8045b93f2dd7dd9d2a70d
Suraj Yadav <suraj.yadav@ingrammicro.com>
2021-01-15 14:02:29 +0530

    #5: Added fillable properties and relations

--- example-app/app/Employee.php
+++ example-app/app/Employee.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    // set fillable properties
+    protected $fillable = [
+        'department_id',
+        'emp_name'
+    ];
+
+    // one to many relationship between department and employee
+    public function department() {
+        return $this->belongsTo('App\Department');
+    }
+
+    // one to many relationship between employee and employeeContact
+    public function empContact() {
+        return $this->hasMany('App\EmployeeContact');
+    }
 }
--- example-app/app/EmployeeContact.php
+++ example-app/app/EmployeeContact.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class EmployeeContact extends Model
 {
-    //
+    // set fillable properties
+    protected $fillable = [
+        'emp_id',
+        'contact_number',
+        'address'
+    ];
+
+    // one to many relationship between employee and employeeContact
+    public function employee() {
+        return $this->belongsTo('App\Employee');
+    }
 }
