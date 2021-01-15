d85639e1b5546252a37b085b23068a7f4b99947d
Suraj Yadav <suraj.yadav@ingrammicro.com>
2021-01-15 10:36:56 +0530

    #3: Added Resource Routes and Controllers file

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/department', DepartmentController::class);
+
+Route::resource('/employee', EmpController::class);
+
+Route::resource('/contacts', ContactsController::class);
